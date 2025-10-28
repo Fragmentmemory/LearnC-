94ee0646282a11ac9b43c8b8105d5479e2c1ce37
Fragmentmemory <1804006798@qq.com>
2025-10-28 23:26:33 +0800

    8.14 Generating random numbers in C++ using Mersenne Twister 使用Mersenne Twister在C++中生成随机数

--- main.cpp
+++ main.cpp
@@ -1,35 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include <random>
 
-unsigned int s_state{ 0 };
+int main()
+{
+	std::mt19937 mt{}; 
 
-void seedPRNG(unsigned int seed) {
-	// 使用给定的种子初始化状态值
-	s_state = seed;
-}
+	for (int count{1};count <= 40; ++count)
+	{
+		std::cout << mt() << '\t';
 
-unsigned int LCG16() {
-	// 生成下一个伪随机数
-
-	// 我们用很大的常量修改状态值使其超出16位范围
-	s_state = 8253729 * s_state + 2396403;
-	return s_state % 32768; // 返回16位范围内的值（0到32767）
-}
-
-void print10() {
-	// 打印10个伪随机数
-	for(int count{1};count <= 10; ++count) {
-		std::cout << LCG16() << '\t';
+		if(count % 5 == 0)
+			std::cout << '\n';
 	}
-	std::cout << std::endl;
-}
-
-int main() {
-	unsigned int x{};
-	std::cout << "Enter seed value: ";
-	std::cin >> x;
-
-	seedPRNG(x);
-	print10();
-
 	return 0;
 }
